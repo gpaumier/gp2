fd90c0ca518f327fe2e3d2ca6dacbab9fc33756e
Guillaume Paumier <gp@guillaumepaumier.com>
2015-06-11 19:31:01 -0700

    add dates with momentjs

--- build.js
+++ build.js
@@ -1,14 +1,16 @@
-var metalsmith = require('metalsmith');
-var markdown   = require('metalsmith-pandoc');
-var templates  = require('metalsmith-templates');
-var serve      = require('metalsmith-serve');
-var watch      = require('metalsmith-watch')
+var metalsmith  = require('metalsmith');
+var markdown    = require('metalsmith-pandoc');
+var templates   = require('metalsmith-templates');
+var serve       = require('metalsmith-serve');
+var watch       = require('metalsmith-watch');
+var moment      = require('moment');
 
 metalsmith(__dirname)
     .source('src')
     .use(markdown())
     .use(templates({
-        engine: 'jade'
+        engine: 'jade',
+        moment: moment
     }))
     .destination('build')
     .use(serve({
