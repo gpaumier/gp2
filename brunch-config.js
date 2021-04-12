3074a725b3bec970c2b826117e907e1d2775479d
Guillaume Paumier <guillaume.paumier@gmail.com>
2021-04-12 19:40:06 -0400

    update brunch config to new format

--- brunch-config.js
+++ brunch-config.js
@@ -1,44 +1,43 @@
 module.exports = {
-  config: {
-    optimize: true,
-    files: {
-      javascripts: {
-        joinTo: {
-          'libs.js': /^node_modules/,
-          'fumseck.js': /^bits/
-        }
-      },
-      stylesheets: {
-        joinTo: 'fumseck.css'
+  optimize: true,
+  files: {
+    javascripts: {
+      joinTo: {
+        'libs.js': /^node_modules/,
+        'fumseck.js': /^bits/
       }
     },
-    paths: {
-      watched: ['bits'],
-      public: 'themes/fumseck/assets'
-    },
-    plugins: {
-      sass: {
-        options: {
-          includePaths: ['node_modules/foundation-sites/scss', 'node_modules/font-awesome/scss']
-        }
-      },
-      postcss: {
-        processors: [
-          require('autoprefixer')({
-            // per https://foundation.zurb.com/sites/docs/sass.html#autoprefixer-required
-            browsers: ['last 2 versions',
-          'ie >= 9',
-          'and_chr >= 2.3']
-          }),
-          require('csswring')
-        ]
-      },
-      copycat: {
-        "fonts/fontawesome": ["node_modules/font-awesome/fonts"]
-      }
-    },
-    npm: {
-      compilers: ['babel-brunch']
+    stylesheets: {
+      joinTo: 'fumseck.css'
     }
+  },
+  paths: {
+    watched: ['bits'],
+    public: 'themes/fumseck/assets'
+  },
+  plugins: {
+    sass: {
+      // https://github.com/brunch/sass-brunch
+      options: {
+        includePaths: ['node_modules/foundation-sites/scss', 'node_modules/font-awesome/scss'],
+        precision: 8,
+        allowCache: true,
+        sourceMapEmbed: true
+      }
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')(
+          // per https://get.foundation/sites/docs/sass.html#autoprefixer-required
+          ['last 2 versions', 'ie >= 9', 'android >= 4.4', 'ios >= 7']),
+        require('cssnano')
+      ]
+    },
+    copycat: {
+      "fonts/fontawesome": ["node_modules/font-awesome/fonts"]
+    }
+  },
+  npm: {
+    compilers: ['babel-brunch']
   }
 };
